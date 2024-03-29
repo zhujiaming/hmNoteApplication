0bd6562adf397bf18b182de72556fc35cf4a3ac5
zhujm <ah_zjm@163.com>
2024-03-29 11:02:52 +0800

    update

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,6 +1,7 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
+import relationalStore from '@ohos.data.relationalStore';
 
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
@@ -14,6 +15,27 @@ export default class EntryAbility extends UIAbility {
   onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+
+    // DB INIT
+    const STORE_CONFIG = {
+      name: 'RdbTest.db', // 数据库文件名
+      securityLevel: relationalStore.SecurityLevel.S1 // 数据库安全级别
+    };
+
+    const SQL_CREATE_TABLE = 'CREATE TABLE IF NOT EXISTS NOTE (ID INTEGER PRIMARY KEY AUTOINCREMENT, CONTENT TEXT)'; // 建表Sql语句
+
+    relationalStore.getRdbStore(this.context, STORE_CONFIG, (err, store) => {
+      if (err) {
+        console.error(`Failed to get RdbStore. Code:${err.code}, message:${err.message}`);
+        return;
+      }
+      console.info(`Succeeded in getting RdbStore.`);
+      store.executeSql(SQL_CREATE_TABLE); // 创建数据表
+
+      // 这里执行数据库的增、删、改、查等操作
+      globalThis.dbStore = store
+    });
+
 
     windowStage.loadContent('pages/Index', (err, data) => {
       if (err.code) {
